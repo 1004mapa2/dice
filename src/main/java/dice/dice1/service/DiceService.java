d96a910a5d771c24c1ff7e7b9706d1a965153cb2
phj <1004mapa2@naver.com>
2023-05-29 01:10:36 +0900

    dice sevice 개발

---
+++ src/main/java/dice/dice1/service/DiceService.java
@@ -0,0 +1,43 @@
+package dice.dice1.service;
+
+import dice.dice1.domain.DiceDomain;
+import dice.dice1.repository.DiceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class DiceService {
+
+    private final DiceRepository diceRepository;
+
+    public DiceDomain DiceNumberCreate() {
+
+        DiceDomain diceDomain = new DiceDomain();
+        diceDomain.setDiceNumber((int) (Math.random() * 6 + 1));
+
+        return diceDomain;
+    }
+
+    public void lastNumberSave(DiceDomain diceDomain) {
+        diceRepository.lastNumberSave(diceDomain);
+    }
+
+    public void diceStoreSave(DiceDomain diceDomain) {
+        diceRepository.storeSave(diceDomain);
+    }
+
+    public Integer lastNumberGet() {
+        return diceRepository.lastNumberGet();
+    }
+
+    public Map<Integer, Integer> diceStoreGet() {
+        return diceRepository.storeGet();
+    }
+
+    public void diceStoreClear() {
+        diceRepository.clearStore();
+    }
+}
